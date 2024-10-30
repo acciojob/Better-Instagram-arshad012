7ebbea70cedc18c18c02eaae2105d89f789801ae
arshad012 <forarshad99@gmail.com>
2024-10-30 14:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let currentDiv = null;
+function drag(e) {
+	currentDiv = e.target;
+}
+
+function allowDrop(e) {
+	e.preventDefault();
+}
+
+function drop(e) {
+	let targetDiv = e.currentTarget;
+	let tempDiv = targetDiv.id;
+	targetDiv.id = currentDiv.id;
+	currentDiv.id = tempDiv;
+}
